47c7c6a0f6782bf80ca69b0c66b301ad729804cb
De <spark49942@gmail.com>
2023-01-17 04:23:44 +0300

    DZ-dayone

---
+++ Divide.java
@@ -0,0 +1,5 @@
+public class Plus {
+    public void sum(double dig1, double dig2, String[] args) {
+        
+    }
+}
---
+++ Minus.java
@@ -0,0 +1,7 @@
+public class Minus extends Plus implements Calcula 
+{
+    public Minus(double dig1, double dig2, String[] args) 
+    {
+        System.out.print(dig1-dig2);
+    }
+}
